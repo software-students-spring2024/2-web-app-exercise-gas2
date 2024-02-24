67b3a18343739e317dabcb477b03a61cd34a1768
al6862 <al6862@nyu.edu>
2024-02-24 17:29:46 -0500

    added routes to view all decks, to create a new deck, to add new card to existing deck

--- app.py
+++ app.py
@@ -26,6 +26,31 @@ except Exception as e:
 def home():
     return render_template('mainScreen.html')
 
+@app.route("/<username>/decks")
+def allDecks(username):
+    # would need to first find user in db, but not set up yet
+    # would redirect to template for Decks
+    return f'{username} decks'
+
+@app.route("/<username>/create", methods=["POST"])
+def createDeck(username):
+    # would need to first find user in db, but not set up yet
+    title = request.form["title"]
+    newDeck = {"title": title, "cards": []}
+    db.decks.insert_one(newDeck)
+    # would rendirect to template for Cards
+    return "created deck"
+
+@app.route("/<username>/<deckTitle>/add", methods=["POST"])
+def addCard(username, deckTitle):
+    # would need to first find user in db, but not set up yet
+    deck = db.decks.find_one({"title": deckTitle})
+    newCard = request.form["question"]
+    deck["cards"].append(newCard)
+    db.decks.update_one({"title": deckTitle}, {"$set": deck})
+    # would redirect to template for Cards
+    return "added card"
+
 # run the app
 if __name__ == "__main__":
     FLASK_PORT = os.getenv("FLASK_PORT", "5000")
