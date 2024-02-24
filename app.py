b1c7963b01be6cf8a0cafca4064519e76aa0708c
iltenahmet <iltenahmet@gmail.com>
2024-02-24 18:42:27 -0500

    Display error message if .env is not set up. Update pipfile to include dotenv

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 uri = os.getenv("MONGO_URI")
 db_name = os.getenv("MONGO_DBNAME")
 
+# Display error message if mongoDB environment is not set up
+if not uri or not db_name:
+    error_message = "MongoDB environment is not set up. Please check your environment variables."
+    app.logger.error(error_message)
+    raise EnvironmentError(error_message)
+
 # Create a new client and connect to the server
 client = MongoClient(uri)
 db = client[db_name]
