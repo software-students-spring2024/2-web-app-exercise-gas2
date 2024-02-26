c673f5202deedf6870a7e74ea402dcf5f8764f50
iltenahmet <iltenahmet@gmail.com>
2024-02-26 11:52:38 -0500

    Move db realted stuff to its own file

--- authentication.py
+++ authentication.py
@@ -1,26 +1,28 @@
 from flask import render_template, request, redirect, url_for, session
 from flask_login import LoginManager
-import app
+import db
 
 
 login_manager = LoginManager()
 
 class User():
-    def __init__(self, is_authenticated, is_active, is_anonymous):
+    def __init__(self, username, password, is_authenticated, is_active, is_anonymous):
         self.is_authenticated = is_authenticated
         self.is_active = is_active
         self.is_anonymous = is_anonymous
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
-def login(db):
-    print(db)
+def login():
     if request.method == 'POST':
         ## TODO: Login
         return "Someone pressed the login button huh, I better log you in."
     else:
         return render_template('login.html')
 
-def signup(db):
+def signup():
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
